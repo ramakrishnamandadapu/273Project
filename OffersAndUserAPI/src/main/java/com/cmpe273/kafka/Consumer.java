7af3579917637a0d85606867775dd8a503aab81d
ramakrishnamandadapu <ramakrishna.mandadapu@sjsu.edu>
2015-05-11 00:50:16 -0700

    added mailing api code

--- OffersAndUserAPI/src/main/java/com/cmpe273/kafka/Consumer.java
+++ OffersAndUserAPI/src/main/java/com/cmpe273/kafka/Consumer.java
@@ -1,6 +1,5 @@
 package com.cmpe273.kafka;
 
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,39 +10,40 @@ import kafka.consumer.ConsumerIterator;
 import kafka.consumer.KafkaStream;
 import kafka.javaapi.consumer.ConsumerConnector;
 
-public class Consumer  {
-    private final ConsumerConnector consumer;
-    private final String topic;
+import com.cmpe273.mail.MailHandler;
 
-    public Consumer()
-    {
-        consumer = kafka.consumer.Consumer.createJavaConsumerConnector(
-                createConsumerConfig());
-        this.topic = "test";
-    }
+public class Consumer {
+	private final ConsumerConnector consumer;
+	private final String topic;
+	private MailHandler mailHandler=new MailHandler();
 
-    private static ConsumerConfig createConsumerConfig()
-    {
-        Properties props = new Properties();
-        props.put("zookeeper.connect", KafkaProperties.zkConnect);
-        props.put("group.id", KafkaProperties.groupId);
-        props.put("zookeeper.session.timeout.ms", "400");
-        props.put("zookeeper.sync.time.ms", "200");
-        props.put("auto.commit.interval.ms", "1000");
+	public Consumer() {
+		consumer = kafka.consumer.Consumer
+				.createJavaConsumerConnector(createConsumerConfig());
+		this.topic = "test";
+	}
 
-        return new ConsumerConfig(props);
+	private static ConsumerConfig createConsumerConfig() {
+		Properties props = new Properties();
+		props.put("zookeeper.connect", KafkaProperties.zkConnect);
+		props.put("group.id", KafkaProperties.groupId);
+		props.put("zookeeper.session.timeout.ms", "400");
+		props.put("zookeeper.sync.time.ms", "200");
+		props.put("auto.commit.interval.ms", "1000");
 
-    }
+		return new ConsumerConfig(props);
 
-    public void receive() {
-        Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
-        topicCountMap.put(topic, new Integer(1));
-        Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap = consumer.createMessageStreams(topicCountMap);
-        KafkaStream<byte[], byte[]> stream =  consumerMap.get(topic).get(0);
-        ConsumerIterator<byte[], byte[]> it = stream.iterator();
-        while(it.hasNext())
-            System.out.println("Received Msg: "+new String(it.next().message()));
-    }
-    
-   
+	}
+
+	public void receive() {
+		Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
+		topicCountMap.put(topic, new Integer(1));
+		Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap = consumer
+				.createMessageStreams(topicCountMap);
+		KafkaStream<byte[], byte[]> stream = consumerMap.get(topic).get(0);
+		ConsumerIterator<byte[], byte[]> it = stream.iterator();
+		while (it.hasNext())
+			mailHandler.sendMessage(new String(it.next().message()));
+	}
+
 }
---
+++ OffersAndUserAPI/src/main/java/com/cmpe273/mail/MailHandler.java
@@ -0,0 +1,52 @@
+package com.cmpe273.mail;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.stereotype.Component;
+
+public class MailHandler {
+	
+	public void sendMessage(String messageString) {
+		String[] splitMessage = messageString.split(";");
+		 
+		final String username = "cmpe273proj@gmail.com";
+		final String password = "273Projectuser";
+ 
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+ 
+		Session session = Session.getInstance(props,
+		  new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		  });
+ 
+		try {
+ 
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(username));
+			message.setRecipients(Message.RecipientType.TO,
+				InternetAddress.parse(splitMessage[0]));
+			message.setSubject("Offers in Emart");
+			message.setText(splitMessage[1]);
+ 
+			Transport.send(message);
+ 
+ 
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
